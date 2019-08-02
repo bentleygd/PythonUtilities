7b0cb70239df00e0cd1bfd4a20b2bfa4903b7ad4
Gabriel Bentley <gbentley@24hourfit.com>
2019-08-02 14:39:01 -0700

    new file:   .gitignore 	new file:   GetMailSample.py 	new file:   coreutils.py

---
+++ GetMailSample.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+from ldap import initialize,SCOPE_SUBTREE
+from random import sample
+from csv import DictWriter
+import coreutils
+
+
+def GetADMailUsers(ldap_url, bind_dn, passw, ous):
+    """Retrieves email addresses from AD."""
+    mail_list = []
+    ldap_obj = initialize(ldap_url)
+    ldap_obj.simple_bind_s(bind_dn, passw)
+    for ou in ous:
+        user_data = (ldap_obj.search_s(ou, SCOPE_SUBTREE, 'mail=*',
+                     ['givenName','sn','mail','physicalDeliveryOfficeName',
+                      'department'], attrsonly=0))
+        for data in user_data:
+            mail_list.append(data[1])
+    return mail_list
+
+
+def GetMailSample(population):
+    """Returns a 10% sample of users with email address."""
+    pop_size = len(population)
+    sample_size = int(round(pop_size / 10))
+    sample_list = sample(population, sample_size)
+    return sample_list
+
+
+# Setting configuration file.
+ml_smpl_cnf = coreutils.GetConfig('mail-sample.cnf')
+# Getting GnuPG info
+gpghome = ml_smpl_cnf.GPGHome()
+gpg_pass = str(open(ml_smpl_cnf.GPGPass(), 'r').read()).strip('\n')
+
+# Setting LDAP Info
+l_url = ml_smpl_cnf.LDAP_URL()
+l_bdn = ml_smpl_cnf.LDAP_BDN()
+l_pass = str(coreutils.DecryptGPG(ml_smpl_cnf.LDAP_Pass(), gpghome, gpg_pass)
+             ).strip('\n')
+l_ous = ml_smpl_cnf.LDAPSearchOU()
+
+# Getting user data
+mail_list = GetADMailUsers(l_url,l_bdn,l_pass,l_ous)
+mail_sample = GetMailSample(mail_list)
+
+# Writing data to a CSV file
+results_file = ml_smpl_cnf.ResultsFile()
+csv_file = open(results_file, 'w+b')
+f_names = ['First Name', 'Last Name', 'email', 'Department', 'Region']
+writer = DictWriter(csv_file, fieldnames=f_names, dialect='excel')
+writer.writeheader()
+for user in mail_sample:
+    user = dict(user)
+    print user
+    print user.get('mail')[0]
+    print user.get('givenName')[0]
+    print user.get('sn')[0]
+    writer.writerow({'First Name': user.get('givenName')[0],
+                     'Last Name': user.get('sn')[0],
+                     'email': user.get('mail')[0],
+                     'Department': user.get('department')[0],
+                     'Region': user.get('physicalDeliveryOfficeName')[0]})
+csv_file.close()
---
+++ coreutils.py
@@ -0,0 +1,129 @@
+from email.mime.text import MIMEText
+from socket import gethostbyname
+from re import search
+from gnupg import GPG
+from smtplib import SMTP
+import logging
+
+
+def SysLogSetup():
+    """Function to be used to set up simple syslog logging."""
+    syslogme = logging.GetLogger('SysLogger')
+    syslogme.setLevel(logging.DEBUG)
+
+    # Setting up a syslog handler.  Change the log level and facility
+    # if desired.
+    sl = logging.SysLogHandler()
+    sl.setLevel(logging.INFO)
+
+    # Creating a log formatter.
+    sl_format = logging.Formatter('%(asctime)s - %(processName)s -\
+                                   %(levelname)s - %(message)s',
+                                  datefmt='%m/%d/%Y %I:%M:%S %p')
+    sl.setFormatter(sl_format)
+    syslogme.addHandler(sl)
+
+
+def FileLogSetup():
+    """Function to be used to set up simple logging to a file."""
+    filelogme = logging.GetLogger('FileLogger')
+    filelogme.setLevel(loogging.DEBUG)
+
+    # Setting up a rotating file log handler.
+    rfh = logging.RotatingFileHandler('/somelogname', maxBytes=52428800,
+                                      backupCount=5)
+    rfh.setLevel(logging.DEBUG)
+    rfh_format = logging.Formatter('%(asctime)s - %(processName)s -\
+                                   %(levelname)s - %(message)s', exc_info,
+                                   datefmt='%m/%d/%Y %I:%M:%S %p')
+    rfh.setFormatter(rfh_format)
+    filelogme.addHandler(rfh)
+
+
+class GetConfig:
+    """A configuration class"""
+    def __init__(self, file_location):
+        self.fl = file_location
+
+    def GPGHome(self):
+        """Gets gpg's home dir from config file."""
+        config_file = open(self.fl, 'r+b')
+        for line in config_file:
+            gpg_rgx = search(r'(^GNUPGHOME = )(.+)', line)
+            if gpg_rgx:
+                return gpg_rgx.group(2).strip()
+        config_file.close()
+
+    def GPGPass(self):
+        """Gets the location of the gpg password."""
+        config_file = open(self.fl, 'r+b')
+        for line in config_file:
+            gpg_pass_rgx = search(r'(^GPGPASS = )(.+)', line)
+            if gpg_pass_rgx:
+                return gpg_pass_rgx.group(2).strip()
+        config_file.close()
+
+    def LDAP_BDN(self):
+        """Gets an LDAP Bind DN from the config file."""
+        config_file = open(self.fl, 'r+b')
+        for line in config_file:
+            bdn_rgx = search(r'(^LDAP_BDN: )(.+)', line)
+            if bdn_rgx:
+                return bdn_rgx.group(2).strip()
+        config_file.close()
+
+    def LDAP_URL(self):
+        """Gets the LDAP URL to connect to."""
+        config_file = open(self.fl, 'r+b')
+        for line in config_file:
+            lurl_rgx = search(r'(^LDAP_URL: )(.+)', line)
+            if lurl_rgx:
+                return lurl_rgx.group(2).strip()
+        config_file.close()
+
+    def LDAP_Pass(self):
+        """Gets the password for an LDAP connection."""
+        config_file = open(self.fl, 'r+b')
+        for line in config_file:
+            pass_rgx = search(r'(^PASS_FILE: )(.+)', line)
+            if pass_rgx:
+                return pass_rgx.group(2).strip()
+        config_file.close()
+
+    def LDAPSearchOU(self):
+        """Gets a list of OUs to search through."""
+        config_file = open(self.fl, 'r+b')
+        for line in config_file:
+            ou_rgx = search(r'(^SEARCH_OUs: )(.+)', line)
+            if ou_rgx:
+                search_ou = ou_rgx.group(2).split('|')
+                return search_ou
+        config_file.close()
+
+    def ResultsFile(self):
+        """Gets the location of the results file."""
+        config_file = open(self.fl, 'r+b')
+        for line in config_file:
+            rf_rgx = search(r'(RESULTS_CSV: )(.+)', line)
+            if rf_rgx:
+                results_file = rf_rgx.group(2)
+                return results_file
+        config_file.close()
+
+
+def MailSend(mail_sender, mail_recipients, mail_server, mail_body):
+    """Simple function to send mail."""
+    msg = MIMEText(mail_body)
+    msg['Subject'] = 'MSFT IP Scrape'
+    msg['From'] = mail_sender
+    msg['To'] = mail_recipients
+    s = SMTP(gethostbyname(mail_server), '25')
+    s.sendmail(mail_sender, mail_recipients, msg.as_string())
+
+
+def DecryptGPG(cipher_file, gpghome, p_phrase):
+    """Simple decrypt."""
+    cipher_data = str(open(cipher_file, 'r').read()).strip('\n')
+    g = GPG(gnupghome=gpghome)
+    clear_data = g.decrypt(cipher_data, passphrase=p_phrase)
+    return clear_data
